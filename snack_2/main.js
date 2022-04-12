a8940db93a6612d0e5047052d5f11711f6c71d97
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-12 11:45:56 +0200

    add snack_2

---
+++ snack_2/main.js
@@ -0,0 +1,25 @@
+/* 
+    Crea 10 oggetti che rappresentano una zucchina.
+    Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+    Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+*/
+
+const zucchine = [
+    { varieta: "Faenza", peso: 10, lunghezza: 15 },
+    { varieta: "Faenza", peso: 30, lunghezza: 10 },
+    { varieta: "Faenza", peso: 40, lunghezza: 12 },
+    { varieta: "Faenza", peso: 50, lunghezza: 22 },
+    { varieta: "Faenza", peso: 60, lunghezza: 14 },
+    { varieta: "Faenza", peso: 45, lunghezza: 12 },
+    { varieta: "Faenza", peso: 30, lunghezza: 5 },
+    { varieta: "Faenza", peso: 15, lunghezza: 25 },
+    { varieta: "Faenza", peso: 22, lunghezza: 7 },
+    { varieta: "Faenza", peso: 32, lunghezza: 9 },
+]
+
+const zucchineLunghe = zucchine.filter(zucchina => zucchina.lunghezza < 15)
+console.log(zucchineLunghe)
+
+const zucchineCorte = zucchine.filter(zucchina => zucchina.lunghezza > 15)
+console.log(zucchineCorte)
