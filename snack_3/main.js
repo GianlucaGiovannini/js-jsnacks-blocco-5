905eba07c5a97e8f57e193c7d0e58e3b771a383a
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-12 11:59:29 +0200

    add function comment

--- snack_3/main.js
+++ snack_3/main.js
@@ -2,6 +2,11 @@
     Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
  */
 
+/**
+ * ## Invertitore di parole
+ * @param {string} word scrivi la parola che vuoi invertire
+ * @returns {string} parola invertita
+ */
 function reverse(word) {
     const wordReverse = word.split("").reverse().join("")
     return wordReverse
